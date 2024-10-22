ca29726df1612ac5a3fcad1dcbea6a5aea19dda9
skerlvn <skerlvn@gmail.com>
2024-10-22 15:39:54 +0300

    Добавлена ЛР6, реализован класс MyVector. Создан пустой проект для ЛР7

---
+++ Lab6/MyVector.cs
@@ -0,0 +1,260 @@
+﻿namespace Lab6
+{
+    public class MyVector<T>
+    {
+        private T[] elementData;     // Массив для хранения элементов вектора
+        private int elementCount;    // Количество элементов в векторе
+        private int capacityIncrement; // Величина приращения ёмкости при необходимости
+
+        public MyVector(int initialCapacity, int capacityIncrement)
+        {
+            if (initialCapacity < 0) throw new ArgumentException("Capacity must be non-negative.");
+            elementData = new T[initialCapacity];
+            this.capacityIncrement = capacityIncrement;
+            elementCount = 0;
+        }
+
+        public MyVector(int initialCapacity) : this(initialCapacity, 0) { }
+
+        public MyVector() : this(10, 0) { }
+
+        public MyVector(T[] a)
+        {
+            elementData = new T[a.Length];
+            Array.Copy(a, elementData, a.Length);
+            elementCount = a.Length;
+            capacityIncrement = 0;
+        }
+
+        public void Add(T e)
+        {
+            EnsureCapacity(elementCount + 1);
+            elementData[elementCount++] = e;
+        }
+
+        public void AddAll(T[] a)
+        {
+            EnsureCapacity(elementCount + a.Length);
+            Array.Copy(a, 0, elementData, elementCount, a.Length);
+            elementCount += a.Length;
+        }
+
+        public void Clear()
+        {
+            Array.Clear(elementData, 0, elementCount);
+            elementCount = 0;
+        }
+
+        public bool Contains(object o)
+        {
+            return IndexOf(o) >= 0;
+        }
+
+        public bool ContainsAll(T[] a)
+        {
+            foreach (T item in a)
+            {
+                if (!Contains(item)) return false;
+            }
+            return true;
+        }
+
+        public bool IsEmpty()
+        {
+            return elementCount == 0;
+        }
+
+        public bool Remove(object o)
+        {
+            int index = IndexOf(o);
+            if (index >= 0)
+            {
+                Remove(index);
+                return true;
+            }
+            return false;
+        }
+
+        public void RemoveAll(T[] a)
+        {
+            foreach (T item in a)
+            {
+                Remove(item);
+            }
+        }
+
+        public void RetainAll(T[] a)
+        {
+            for (int i = elementCount - 1; i >= 0; i--)
+            {
+                if (Array.IndexOf(a, elementData[i]) < 0)
+                {
+                    Remove(i);
+                }
+            }
+        }
+
+        public int Size()
+        {
+            return elementCount;
+        }
+
+        public T[] ToArray()
+        {
+            T[] result = new T[elementCount];
+            Array.Copy(elementData, result, elementCount);
+            return result;
+        }
+
+        public T[] ToArray(T[] a)
+        {
+            if (a == null || a.Length < elementCount)
+            {
+                return ToArray();
+            }
+            Array.Copy(elementData, a, elementCount);
+            if (a.Length > elementCount)
+            {
+                a[elementCount] = default(T);
+            }
+            return a;
+        }
+
+        public void Add(int index, T e)
+        {
+            if (index < 0 || index > elementCount) throw new ArgumentOutOfRangeException();
+            EnsureCapacity(elementCount + 1);
+            Array.Copy(elementData, index, elementData, index + 1, elementCount - index);
+            elementData[index] = e;
+            elementCount++;
+        }
+
+        public void AddAll(int index, T[] a)
+        {
+            if (index < 0 || index > elementCount) throw new ArgumentOutOfRangeException();
+            EnsureCapacity(elementCount + a.Length);
+            Array.Copy(elementData, index, elementData, index + a.Length, elementCount - index);
+            Array.Copy(a, 0, elementData, index, a.Length);
+            elementCount += a.Length;
+        }
+
+        public T Get(int index)
+        {
+            if (index < 0 || index >= elementCount) throw new ArgumentOutOfRangeException();
+            return elementData[index];
+        }
+
+        public int IndexOf(object o)
+        {
+            if (o == null)
+            {
+                for (int i = 0; i < elementCount; i++)
+                {
+                    if (elementData[i] == null) return i;
+                }
+            }
+            else
+            {
+                for (int i = 0; i < elementCount; i++)
+                {
+                    if (o.Equals(elementData[i])) return i;
+                }
+            }
+            return -1;
+        }
+
+        public int LastIndexOf(object o)
+        {
+            if (o == null)
+            {
+                for (int i = elementCount - 1; i >= 0; i--)
+                {
+                    if (elementData[i] == null) return i;
+                }
+            }
+            else
+            {
+                for (int i = elementCount - 1; i >= 0; i--)
+                {
+                    if (o.Equals(elementData[i])) return i;
+                }
+            }
+            return -1;
+        }
+
+        public T Remove(int index)
+        {
+            if (index < 0 || index >= elementCount) throw new ArgumentOutOfRangeException();
+            T removedElement = elementData[index];
+            int numMoved = elementCount - index - 1;
+            if (numMoved > 0)
+            {
+                Array.Copy(elementData, index + 1, elementData, index, numMoved);
+            }
+            elementData[--elementCount] = default(T);
+            return removedElement;
+        }
+
+        public T Set(int index, T e)
+        {
+            if (index < 0 || index >= elementCount) throw new ArgumentOutOfRangeException();
+            T oldValue = elementData[index];
+            elementData[index] = e;
+            return oldValue;
+        }
+
+        public MyVector<T> SubList(int fromIndex, int toIndex)
+        {
+            if (fromIndex < 0 || toIndex > elementCount || fromIndex > toIndex)
+                throw new ArgumentOutOfRangeException();
+
+            MyVector<T> sublist = new MyVector<T>(toIndex - fromIndex);
+            for (int i = fromIndex; i < toIndex; i++)
+            {
+                sublist.Add(elementData[i]);
+            }
+            return sublist;
+        }
+
+        public T FirstElement()
+        {
+            if (elementCount == 0) throw new InvalidOperationException("Vector is empty.");
+            return elementData[0];
+        }
+
+        public T LastElement()
+        {
+            if (elementCount == 0) throw new InvalidOperationException("Vector is empty.");
+            return elementData[elementCount - 1];
+        }
+
+        public void RemoveElementAt(int pos)
+        {
+            Remove(pos);
+        }
+        public void RemoveRange(int begin, int end)
+        {
+            if (begin < 0 || end > elementCount || begin > end) throw new ArgumentOutOfRangeException();
+            int numRemoved = end - begin;
+            Array.Copy(elementData, end, elementData, begin, elementCount - end);
+            Array.Clear(elementData, elementCount - numRemoved, numRemoved); // Обнуляем удалённые элементы
+            elementCount -= numRemoved;
+        }
+
+        private void EnsureCapacity(int minCapacity)
+        {
+            if (minCapacity > elementData.Length)
+            {
+                int newCapacity = (capacityIncrement > 0)
+                    ? elementData.Length + capacityIncrement
+                    : elementData.Length * 2;
+
+                if (newCapacity < minCapacity) newCapacity = minCapacity;
+                T[] newArray = new T[newCapacity];
+                Array.Copy(elementData, newArray, elementCount);
+                elementData = newArray;
+            }
+        }
+    }
+
+}
---
+++ Lab6/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            MyVector<int> vector = new MyVector<int>();
+
+            Console.WriteLine("Добавление элементов в вектор:");
+            vector.Add(1);
+            vector.Add(2);
+            vector.Add(3);
+            PrintVector(vector);
+
+            Console.WriteLine($"\nРазмер вектора: {vector.Size()}");
+
+
+            Console.WriteLine($"\nСодержит ли вектор элемент 2? {vector.Contains(2)}");
+            Console.WriteLine($"Содержит ли вектор элемент 5? {vector.Contains(5)}");
+
+
+            Console.WriteLine($"\nЭлемент с индексом 1: {vector.Get(1)}");
+
+
+            Console.WriteLine("\nДобавление элемента 5 на позицию 1:");
+            vector.Add(1, 5);
+            PrintVector(vector);
+
+
+            Console.WriteLine("\nУдаление элемента на позиции 2:");
+            vector.Remove(2);
+            PrintVector(vector);
+
+
+            Console.WriteLine("\nУдаление элемента 1:");
+            vector.Remove(o: 1);
+            PrintVector(vector);
+
+
+            Console.WriteLine("\nПолучение подмассива с индексами 0-2:");
+            var subList = vector.SubList(0, 2).ToArray();
+            PrintArray(subList);
+
+            Console.WriteLine("\nОчищаем вектор:");
+            vector.Clear();
+            PrintVector(vector);
+
+            Console.WriteLine($"\nВектор пуст? {vector.IsEmpty()}");
+
+            Console.ReadKey();
+        
+        }
+        static void PrintVector(MyVector<int> vector)
+        {
+            Console.WriteLine("Текущий вектор:");
+            for (int i = 0; i < vector.Size(); i++)
+            {
+                Console.Write(vector.Get(i) + " ");
+            }
+            Console.WriteLine();
+        }
+
+        static void PrintArray(int[] array)
+        {
+            Console.WriteLine("Подмассив:");
+            foreach (var item in array)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
